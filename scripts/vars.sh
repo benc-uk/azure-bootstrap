37640ea862cd58caff3e84808f8dadbcccb49d1c
Ben Coleman <benc.uk@gmail.com>
2019-07-05 14:13:41 +0100

    Unicode is a pain

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -4,6 +4,6 @@ ADO_PROJECT="newproj"
 
 # Maybe change these
 ADO_PROJECT_DESC="Bootstrapped project"
-ADO_PROJECT_PROCESS="Basic"  # Change to: Basic, CMMI or Scrum
-REPO_NAME="bootstrap"
-REPO_SOURCE="https://github.com/benc-uk/foobar.git"
+ADO_PROJECT_PROCESS="Basic"  # Must be one of: Agile, Basic, CMMI or Scrum
+REPO_NAME="$ADO_PROJECT"
+REPO_SOURCE="https://github.com/benc-uk/azure-bootstrap.git"
